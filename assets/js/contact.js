a5f48f0832fd817ac4842e7d827ec23bf93f1061
JakubOdyssey <jgajewski.uk@gmail.com>
2026-06-20 19:58:19 +0100

    Track successful contact form submissions

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,6 +2,8 @@
 *
 * Contact JS
 * @ThemeEaster
+* Updated for ClientFlow lead tracking
+*
 */
 $(function() {
     // Get the form.
@@ -10,46 +12,65 @@ $(function() {
     // Get the messages div.
     var formMessages = $('#form-messages');
 
+    // Track successful lead submission in Google Analytics.
+    // This only runs if the visitor accepted optional cookies
+    // and Google Analytics has been loaded.
+    function trackLeadSubmit() {
+        if (typeof window.gtag === "function") {
+            window.gtag("event", "lead_submit", {
+                event_category: "lead",
+                event_label: "contact_form",
+                form_name: "ClientFlow contact form",
+                page_title: document.title,
+                page_location: window.location.href
+            });
+        }
+    }
+
     // Set up an event listener for the contact form.
-	$(form).submit(function(event) {
-		// Stop the browser from submitting the form.
-		event.preventDefault();
+    $(form).submit(function(event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
 
-		// Serialize the form data.
-		var formData = $(form).serialize();
-		// Submit the form using AJAX.
-		$.ajax({
-			type: 'POST',
-			url: $(form).attr('action'),
-			data: formData
-		})
-		.done(function(response) {
-			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('alert-danger');
-			$(formMessages).addClass('alert-success');
+        // Serialize the form data.
+        var formData = $(form).serialize();
 
-			// Set the message text.
-			$(formMessages).text(response);
+        // Submit the form using AJAX.
+        $.ajax({
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: formData
+        })
+        .done(function(response) {
+            // Make sure that the formMessages div has the 'success' class.
+            $(formMessages).removeClass('alert-danger');
+            $(formMessages).addClass('alert-success');
 
-			// Clear the form.
-			$('#fullname').val('');
-			$('#email').val('');
-			$('#phone').val('');
-			$('#message').val('');
-		})
-		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('alert-success');
-			$(formMessages).addClass('alert-danger');
+            // Set the message text.
+            $(formMessages).text(response);
 
-			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
-		});
+            // Track successful lead submission.
+            trackLeadSubmit();
 
-	});
+            // Clear the form.
+            $('#fullname').val('');
+            $('#email').val('');
+            $('#phone').val('');
+            $('#message').val('');
+        })
+        .fail(function(data) {
+            // Make sure that the formMessages div has the 'error' class.
+            $(formMessages).removeClass('alert-success');
+            $(formMessages).addClass('alert-danger');
+
+            // Set the message text.
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            } else {
+                $(formMessages).text('Oops! An error occurred and your message could not be sent.');
+            }
+        });
+
+    });
 
 });
